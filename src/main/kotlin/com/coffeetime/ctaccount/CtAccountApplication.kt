dd3484cd0434c8a416ca155aa7625d8730afffbb
sangily <sangilyoon.dev@gmail.com>
2024-02-22 01:34:01 +0900

    first commit

---
+++ src/main/kotlin/com/coffeetime/ctaccount/CtAccountApplication.kt
@@ -0,0 +1,11 @@
+package com.coffeetime.ctaccount
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CtAccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<CtAccountApplication>(*args)
+}
---
+++ src/test/kotlin/com/coffeetime/ctaccount/CtAccountApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coffeetime.ctaccount
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CtAccountApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
