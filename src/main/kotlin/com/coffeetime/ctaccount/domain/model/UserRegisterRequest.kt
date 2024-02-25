a70335f5de97ce116f0c08f3a1d903da59ccfb75
sangily <sangilyoon.dev@gmail.com>
2024-02-26 00:32:18 +0900

    refactor: socialID 추가

--- src/main/kotlin/com/coffeetime/ctaccount/domain/model/UserRegisterRequest.kt
+++ src/main/kotlin/com/coffeetime/ctaccount/domain/model/UserRegisterRequest.kt
@@ -2,6 +2,7 @@ package com.coffeetime.ctaccount.domain.model
 
 data class UserRegisterRequest(
     val nickName: String,
+    val socialID: String,
     val profilePictureURL: String? = null,
     val jobs: Iterable<String>,
 )
